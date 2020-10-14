606e6996f3140fb3fe29d02e29bae90eb875ad3c
Shishir Srivastava <shishirsrivastava@Shishirs-MacBook-air.local>
2020-10-14 23:36:11 +0530

    Initial commit

---
+++ build/precache-manifest.0a671cb880f089b369cd1c436498d595.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dd9b6b2bf01841467f0",
+    "url": "/static/css/main.42ac4757.chunk.css"
+  },
+  {
+    "revision": "4dd9b6b2bf01841467f0",
+    "url": "/static/js/main.4dd9b6b2.chunk.js"
+  },
+  {
+    "revision": "ac973cbf29d70f59aa43",
+    "url": "/static/css/1.e618537b.chunk.css"
+  },
+  {
+    "revision": "ac973cbf29d70f59aa43",
+    "url": "/static/js/1.ac973cbf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0f784bf2dcd10078b4aafd3e17f527d",
+    "url": "/index.html"
+  }
+];
